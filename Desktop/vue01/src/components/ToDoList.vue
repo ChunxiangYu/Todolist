175846ac969b47b706fc276733ece8ffdbd3e76e
ChunxiangYu <yu1462007467@gmail.com>
2020-10-06 06:46:00 +0800

    todo list

---
+++ Desktop/vue01/src/components/ToDoList.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="ToDoList" class="ToDoList">
+    <h1 v-html="title"></h1>
+    <div>
+      <input type="text" v-model="newItem">
+      <button @click="addNewItem">Add</button>
+    </div>
+    <ul>
+      <li v-for="item in items" :key="item.id">
+        <!-- 待办事项 -->
+        <span v-text="item.title"></span>
+        <!-- 完成按钮 -->
+        <button v-on:click="toggleFinish(recycleItem)">Done</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ToDoList',
+  data () {
+    return {
+      title: 'ToDoList',
+      items: [],
+      id: 0
+    }
+  }
+}
+</script>
